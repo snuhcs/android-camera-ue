6f5fde428968bf45c9fc24e4edc48eb01ed802bc
Jingyu Lee <dostos10@gmail.com>
2022-03-17 19:04:40 +0900

    Based image format conversion in C++

---
+++ AndroidCamera/Source/AndroidCamera/Public/ImageFormatUtils.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+namespace ImageFormatUtils
+{
+    void YUV420SPToARGB8888(char *input, int width, int height, int *output);
+    int YUV2RGB(int y, int u, int v);
+    void YUV420ToARGB8888(char *yData,
+                          char *uData,
+                          char *vData,
+                          int width,
+                          int height,
+                          int yRowStride,
+                          int uvRowStride,
+                          int uvPixelStride,
+                          int *out);
+}
